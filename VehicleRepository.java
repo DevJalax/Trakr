f637106a6622264bd08c446f0eb2ad36923b0991
Jalakandeshwaran <raonejalagandeswaran@gmail.com>
2024-12-15 21:42:18 +0530

    Delete VehicleRepository.java

--- VehicleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.transportation.repository;
-
-import com.example.transportation.model.Vehicle;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface VehicleRepository extends JpaRepository<Vehicle, Long>, JpaSpecificationExecutor<Vehicle> {
-}
