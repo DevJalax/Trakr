2467d795bf63abe7154aba2c8fc5e887d2fbbdf9
Jalakandeshwaran <raonejalagandeswaran@gmail.com>
2024-12-15 21:41:50 +0530

    Delete StatusUpdateRepository.java

--- StatusUpdateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.transportation.repository;
-
-import com.example.transportation.model.StatusUpdate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StatusUpdateRepository extends JpaRepository<StatusUpdate, Long> {
-}
